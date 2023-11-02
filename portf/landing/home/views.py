fbace0dbe95829dec41a75266880f19274d78441
Emmanuel Arce <andresibarce@gmail.com>
2023-11-01 21:02:30 -0600

    first commit

---
+++ portf/landing/home/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+def home(request):
+    template = loader.get_template("home.html")
+    return HttpResponse(template.render())
+
+def testing(request):
+    template = loader.get_template('testing.html')
+    return HttpResponse(template.render())
+
+def index(request):
+    template = loader.get_template("index.html")
+    return HttpResponse(template.render())
